14675921229c8c652c690e82595caffc08e4bc60
wesclaw <108541397+wesclaw@users.noreply.github.com>
2024-01-03 20:40:10 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,93 +1,93 @@
-const Engine = Matter.Engine,
-  Render = Matter.Render,
-  World = Matter.World,
-  Bodies = Matter.Bodies,
-  MouseConstraint = Matter.MouseConstraint,
-  Mouse = Matter.Mouse;
+// const Engine = Matter.Engine,
+//   Render = Matter.Render,
+//   World = Matter.World,
+//   Bodies = Matter.Bodies,
+//   MouseConstraint = Matter.MouseConstraint,
+//   Mouse = Matter.Mouse;
 
-  const engine = Engine.create();
+//   const engine = Engine.create();
 
-  const render = Render.create({
-  element: document.body,
-  engine: engine,
-  options: {
-    width: 400,
-    height: 500,
-    wireframes: false,
-    background: 'white',
-  },
-});
+//   const render = Render.create({
+//   element: document.body,
+//   engine: engine,
+//   options: {
+//     width: 400,
+//     height: 500,
+//     wireframes: false,
+//     background: 'white',
+//   },
+// });
 
-const bowl = Bodies.rectangle(200, 405, 390, 10, { isStatic: true, render: { fillStyle: 'black' } });
-const rightEdge = Bodies.rectangle(390, 250, 10, 300, { isStatic: true, render: { fillStyle: 'black' } });
-const leftEdge = Bodies.rectangle(10, 250, 10, 300, { isStatic: true, render: { fillStyle: 'black' } });
+// const bowl = Bodies.rectangle(200, 405, 390, 10, { isStatic: true, render: { fillStyle: 'black' } });
+// const rightEdge = Bodies.rectangle(390, 250, 10, 300, { isStatic: true, render: { fillStyle: 'black' } });
+// const leftEdge = Bodies.rectangle(10, 250, 10, 300, { isStatic: true, render: { fillStyle: 'black' } });
 
-const pearImage = new Image();
-pearImage.src = 'images/pear.png';
+// const pearImage = new Image();
+// pearImage.src = 'images/pear.png';
 
-const eggplantImage = new Image();
-eggplantImage.src = 'images/eggplant.png';
+// const eggplantImage = new Image();
+// eggplantImage.src = 'images/eggplant.png';
 
-const fallingPear = Bodies.rectangle(175, -80, 60, 60, {
-  render: {
-    sprite: {
-      texture: pearImage.src,
-      xScale: 0.2,
-      yScale: 0.2,
-    },
-  },
-  restitution: 0.8,
-  angle: Math.random() * Math.PI,
-  friction: 0.7,
-});
+// const fallingPear = Bodies.rectangle(175, -80, 60, 60, {
+//   render: {
+//     sprite: {
+//       texture: pearImage.src,
+//       xScale: 0.2,
+//       yScale: 0.2,
+//     },
+//   },
+//   restitution: 0.8,
+//   angle: Math.random() * Math.PI,
+//   friction: 0.7,
+// });
 
-World.add(engine.world, [bowl, rightEdge, leftEdge, fallingPear]);
+// World.add(engine.world, [bowl, rightEdge, leftEdge, fallingPear]);
 
-const mouse = Mouse.create(render.canvas);
-const mouseConstraint = MouseConstraint.create(engine, {
-  mouse: mouse,
-});
+// const mouse = Mouse.create(render.canvas);
+// const mouseConstraint = MouseConstraint.create(engine, {
+//   mouse: mouse,
+// });
 
-World.add(engine.world, mouseConstraint);
+// World.add(engine.world, mouseConstraint);
 
-engine.world.gravity.y = 0.5;
+// engine.world.gravity.y = 0.5;
 
-Engine.run(engine);
-Render.run(render);
+// Engine.run(engine);
+// Render.run(render);
 
-// Add the eggplant after a delay
-setTimeout(() => {
-  const fallingEggplant = Bodies.rectangle(225, -80, 60, 60, {
-    render: {
-      sprite: {
-        texture: eggplantImage.src,
-        xScale: 0.2,
-        yScale: 0.2,
-      },
-    },
-    restitution: 0.8,
-    angle: Math.random() * Math.PI,
-    friction: 0.7,
-  });
+// // Add the eggplant after a delay
+// setTimeout(() => {
+//   const fallingEggplant = Bodies.rectangle(225, -80, 60, 60, {
+//     render: {
+//       sprite: {
+//         texture: eggplantImage.src,
+//         xScale: 0.2,
+//         yScale: 0.2,
+//       },
+//     },
+//     restitution: 0.8,
+//     angle: Math.random() * Math.PI,
+//     friction: 0.7,
+//   });
 
-  World.add(engine.world, fallingEggplant);
+//   World.add(engine.world, fallingEggplant);
 
-  Matter.Events.on(engine, 'beforeUpdate', function () {
-    // Check if the falling pear has reached the bottom
-    if (fallingPear.position.y >= 550) {
-      engine.world.gravity.y = 0;
-      Matter.Body.setPosition(fallingPear, { x: 200, y: 550 });
-      fallingPear.friction = 0.9;
-    }
+//   Matter.Events.on(engine, 'beforeUpdate', function () {
+//     // Check if the falling pear has reached the bottom
+//     if (fallingPear.position.y >= 550) {
+//       engine.world.gravity.y = 0;
+//       Matter.Body.setPosition(fallingPear, { x: 200, y: 550 });
+//       fallingPear.friction = 0.9;
+//     }
 
-    // Check if the falling eggplant has reached the bottom
-    if (fallingEggplant.position.y >= 550) {
-      engine.world.gravity.y = 0;
-      Matter.Body.setPosition(fallingEggplant, { x: 250, y: 650 });
-      fallingEggplant.friction = 0.9;
-    }
-  });
-}, 1000);
+//     // Check if the falling eggplant has reached the bottom
+//     if (fallingEggplant.position.y >= 550) {
+//       engine.world.gravity.y = 0;
+//       Matter.Body.setPosition(fallingEggplant, { x: 250, y: 650 });
+//       fallingEggplant.friction = 0.9;
+//     }
+//   });
+// }, 1000);
 
 
 
